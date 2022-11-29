1fabe488345d664af660eb101529f8d30033a6b7
Toma <whiteshirt0079@gmail.com>
2022-11-29 23:10:38 +0900

    feat: add search func

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,9 +1,11 @@
 use actix_cors::Cors;
 use actix_web::{
+    get,
     http::{self, StatusCode},
     web, App, HttpResponse, HttpServer, Responder,
 };
-use std::process::Command;
+use actix_web_lab::sse::{self, ChannelStream, Sse};
+use std::{process::Command, sync::Mutex};
 
 async fn youtube(path: web::Path<String>) -> impl Responder {
     let id = path.into_inner();
@@ -24,9 +26,100 @@ async fn youtube(path: web::Path<String>) -> impl Responder {
         .await
         .unwrap();
 
-    web::block(|| std::fs::remove_file("./tmp.mp3").unwrap())
-        .await
-        .unwrap();
+    HttpResponse::build(StatusCode::OK)
+        .content_type("audio/mp3")
+        .body(audio)
+}
+
+pub struct Message {
+    inner: Mutex<Vec<sse::Sender>>,
+}
+
+impl Message {
+    async fn new_client(&self) -> Sse<ChannelStream> {
+        let (tx, rx) = sse::channel(10);
+        tx.send(sse::Data::new("connected")).await.unwrap();
+        self.inner.lock().unwrap().push(tx);
+        rx
+    }
+
+    async fn broadcast(&self, msg: &str) {
+        let clients = self.inner.lock().unwrap();
+
+        let send_futures = clients
+            .iter()
+            .map(|client| client.send(sse::Data::new(msg)));
+
+        let _ = futures::future::join_all(send_futures).await;
+    }
+}
+
+async fn stream(msg: web::Data<Message>) -> impl Responder {
+    let rx = msg.new_client().await;
+    return rx;
+}
+
+async fn send_message(path: web::Path<String>, sender: web::Data<Message>) -> impl Responder {
+    let msg = path.into_inner();
+    sender.broadcast(&msg).await;
+    format!("send {}", msg)
+}
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MusicData {
+    title: String,
+    id: String,
+    thumbnail: String,
+}
+
+impl MusicData {
+    fn new(keyword: &str) -> Vec<Self> {
+        let mut vec = vec![];
+
+        let output = Command::new("yt-dlp")
+            .arg(format!("ytsearch:{}", keyword))
+            .args(["--get-title", "--get-thumbnail", "--get-id"])
+            .output()
+            .unwrap();
+        let output = String::from_utf8(output.stdout).unwrap();
+        let output = output.split('\n').collect::<Vec<_>>();
+
+        for out in output.chunks(4) {
+            vec.push(MusicData {
+                title: out[0].to_string(),
+                id: out[1].to_string(),
+                thumbnail: out[2].to_string(),
+            })
+        }
+
+        vec
+    }
+}
+
+async fn search(path: web::Path<String>) -> impl Responder {
+    web::Json(MusicData::new(&path.into_inner()))
+}
+
+#[get("/download/{id}")]
+async fn download(path: web::Path<String>) -> impl Responder {
+    let id = path.into_inner();
+    println!("{}", id);
+    let url = format!("https://www.youtube.com/watch?v={}", id);
+    let audio = web::block(|| {
+        std::fs::remove_file("./tmp.mp3").ok();
+        Command::new("yt-dlp")
+            .args(["-o", r#"./tmp.mp3"#])
+            .arg("-x")
+            .args(["--audio-format", "mp3"])
+            .arg(url)
+            .output()
+            .unwrap();
+        std::fs::read("./tmp.mp3").unwrap()
+    })
+    .await
+    .unwrap();
 
     HttpResponse::build(StatusCode::OK)
         .content_type("audio/mp3")
@@ -35,7 +128,10 @@ async fn youtube(path: web::Path<String>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let msg = web::Data::new(Message {
+        inner: Mutex::new(Vec::new()),
+    });
+    HttpServer::new(move || {
         let cors = Cors::default()
             .allowed_origin("http://localhost:3000")
             .allowed_methods(vec!["GET", "POST"])
@@ -44,10 +140,18 @@ async fn main() -> std::io::Result<()> {
             .max_age(3600);
 
         App::new()
+            .app_data(msg.clone())
             .wrap(cors)
             .service(web::resource("/youtube/{id}").route(web::get().to(youtube)))
+            .service(web::resource("/search/{keyword}").route(web::get().to(search)))
+            .service(web::resource("/stream").route(web::get().to(stream)))
+            .service(web::resource("/send/{msg}").route(web::get().to(send_message)))
+            .service(download)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+    .unwrap();
+
+    Ok(())
 }
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,42 +2,138 @@ import InputGroup from "react-bootstrap/InputGroup";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
-import {GrDownload} from "react-icons/gr";
-import {useState} from "react";
-import './loading.css';
+import Card from 'react-bootstrap/Card';
+import {MdOutlineTransitEnterexit} from "react-icons/md";
+import {AiOutlineDownload} from "react-icons/ai";
+import {useEffect, useRef, useState} from "react";
+import "./loading.css";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+class MusicData {
+    title: string;
+    id: string;
+    thumbnail: string;
+
+    constructor(title: string, id: string, thumbnail: string) {
+        this.title = title;
+        this.id = id;
+        this.thumbnail = thumbnail;
+    }
+}
+
+async function searchAudio(keyword: string) {
+    const url = "http://localhost:8080" + "/search/" + keyword;
+    const response = await fetch(url);
+
+    const data: MusicData[] = await response.json();
+
+    //const decoder = new TextDecoder();
+    //const newMusicList: MusicData[] = [];
+    //     if (!response.body) {
+    //         throw new Error("response body is null");
+    //     }
+    //     const reader = response.body.getReader();
+    //     while (true) {
+    //         const {done, value} = await reader.read();
+
+    //         if (done) break;
+
+    //         const ret = document.createElement("p");
+    //         const data: MusicData = JSON.parse(decoder.decode(value));
+    //         newMusicList.push(data);
+
+    //         document.body.appendChild(ret);
+    //     }
+    return data[0];
+}
 
 
-function download() {
-    const url = "http://localhost:8080/youtube/6DZjCgxbx5U";
-    fetch(url).then(resp => {console.log(resp); return resp.blob();}).then(blob => {
-        console.log(blob);
-        const audio_element = document.createElement("audio");
-        audio_element.src = URL.createObjectURL(blob)
-        audio_element.controls = true;
-        document.body.appendChild(audio_element)
-    });
+async function downloadMusic(id: string, title: string) {
+    const url = "http://localhost:8080/download/" + id;
+    const response = await fetch(url);
+    const blob = await response.blob();
+    const _url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.style.setProperty("display", "none");
+    a.href = _url;
+    a.download = title + ".mp3";
+    a.click();
+    window.URL.revokeObjectURL(url);
+}
+
+function SearchFromKeyword() {
+    const [keyword, setKeyword] = useState("");
+    const [musicList, setMusicList] = useState<MusicData[]>([]);
+
+    const inputElement: React.MutableRefObject<HTMLInputElement | null> = useRef(null);
+    const buttonElement: React.MutableRefObject<HTMLButtonElement | null> = useRef(null);
+
+    useEffect(() => {
+        inputElement.current?.addEventListener("keydown", (event) => {
+            if (event.key == "Enter") {
+                event.preventDefault();
+                buttonElement.current?.click();
+            }
+        });
+    }, []);
+
+    const items = musicList.map((music, index) =>
+        <li key={index.toString()} style={{listStyleType: "none"}}>
+
+            <Container className="p-0 m-0">
+                <Card>
+                    <Row className="no-gutters align-items-center">
+                        <Col md={3}>
+                            <Card.Img variant="top" src={music.thumbnail} />
+                        </Col>
+                        <Col md={7}>
+                            <Card.Body>
+                                <Card.Title>{music.title}</Card.Title>
+                            </Card.Body>
+
+                        </Col>
+                        <Col md={2}>
+                            <Button variant="primary" onClick={() => downloadMusic(music.id, music.title)}>
+                                <AiOutlineDownload className="react-icons" />
+                            </Button>
+                        </Col>
+                    </Row>
+
+                </Card>
+            </Container>
+
+        </li>
+    );
+    return (
+        <>
+            <InputGroup className="mb-3">
+                <InputGroup.Text id="">Keyword</InputGroup.Text>
+                <Form.Control
+                    placeholder=""
+                    ref={inputElement}
+                    onChange={(e) => setKeyword(e.target.value)}
+                />
+                <Button className="pt-1 pb-2" ref={buttonElement} onClick={async () => {
+                    const newMusic = await searchAudio(keyword);
+                    setMusicList([newMusic]);
+                }}>
+                    <div className="icon">
+                        <MdOutlineTransitEnterexit className="react-icons" />
+                    </div>
+                </Button>
+            </InputGroup>
+            <ul>{items}</ul>
+        </>
+    );
 }
 
 function App() {
-    const [loaded, setLoaded] = useState(false);
-    const [id, setId] = useState<string>();
-
     return (
         <Container>
-            <div className="text-center p-3">
+            <div className="text-center p-2">
                 <h1>Youtube Music Downloader</h1>
-                <InputGroup className="mb-3" >
-                    <InputGroup.Text id="">ID</InputGroup.Text>
-                    <Form.Control
-                        placeholder=""
-                        onChange={e => setId(e.target.value)}
-                    />
-                    <Button className="pt-1 pb-2" onClick={download}>
-                        <div className="icon">
-                            <GrDownload className="react-icons" />
-                        </div>
-                    </Button>
-                </InputGroup >
+                <SearchFromKeyword />
             </div>
         </Container>
     );
